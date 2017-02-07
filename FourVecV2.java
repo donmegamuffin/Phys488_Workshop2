605a63ef8778e8eb77270ff573317de05ee5a4ba
donmegamuffin <donmegamuffin@users.noreply.github.com>
2017-02-07 12:18:17 +0000

    Working state #1
    All  methods now accept 2D arrays
    Main now outputting in a debug manner
    TODO : Full comments; Clean console output from main.

--- FourVecV2.java
+++ FourVecV2.java
@@ -1,63 +1,74 @@
+/*Author: Zachary Humphreys; ID: 200951438
+ *Date: 07/02/17
+ */
 import java.io.*;
+import java.util.Arrays;
+
 class FourVecV2
 {
     static BufferedReader keyboard = new
-	BufferedReader (new InputStreamReader(System.in)) ;
+    BufferedReader (new InputStreamReader(System.in)) ;
     static PrintWriter screen = new PrintWriter( System.out, true);
     
-    
-    
-    // ----------- METHODS ------------------------------  
-    
+    // ----------- METHODS ----------------------------------------------------------------------------------------
     // Method 1: Compiles inputs from user into an array
-    private static double[] ArrayCompiler() throws IOException
+    private static double[][] ArrayCompiler() throws IOException
     {
         //Asks user to input four vector into the form: 123 456 789 012
-        screen.println("Please input the four vector where each four vector has 3 digits and each is separated by a space:\t");
-        String userInput = new String(keyboard.readLine());
         //Pulls values for energy and momentum vectors out of the string and compiles them in an array
-        double[] outputArray = {0,0,0,0};
-        for(int n=1; n <= 4; n++)
+        double[][] arrayOut = {{0,0,0,0},{0,0,0,0}};
+        for(int i=0;i<2;i++)
         {
-            int m = (n*4 - 3);
-            outputArray[n-1] = new Double(userInput.substring(m-1,m+2)).doubleValue();
+            screen.println("FOR FOUR VECTOR: "+(i+1));
+            screen.println("Please input the four vector where each four vector has 3 digits and each is separated by a space:\t");
+            String userInput = new String(keyboard.readLine());
+            for(int n=1; n <= 4; n++)
+            {
+                int m = (n*4 - 3);
+                arrayOut[i][n-1] = new Double(userInput.substring(m-1,m+2)).doubleValue();
+            }
         }
-        return outputArray;
+        return arrayOut;
     }
     
     //Method 2: Takes an inputted Array and calculates the Rest Mass and Invariant Momentum
-    private static double[] ArrayMandPextractor(double[] inputArray)
+    private static double[][] ArrayMandPextractor(double[][] arrayIn)
     {
-        double AbsMomentum = Math.sqrt(Math.pow(inputArray[1],2) + Math.pow(inputArray[2],2) + Math.pow(inputArray[3],2));
-        double InvariantMass = Math.sqrt(Math.pow(inputArray[0],2)- Math.pow(AbsMomentum,2));
-        double[] outputArray = {AbsMomentum,InvariantMass};
-        return outputArray;
+        double[][] arrayOut = {{0,0},{0,0}};
+        for(int i=0;i<2;i++)
+        {
+            double AbsMomentum = Math.sqrt(Math.pow(arrayIn[i][1],2) + Math.pow(arrayIn[i][2],2) + Math.pow(arrayIn[i][3],2));
+            double InvariantMass = Math.sqrt(Math.pow(arrayIn[i][0],2)- Math.pow(AbsMomentum,2));
+            arrayOut[i][0] = InvariantMass;
+            arrayOut[i][1] = AbsMomentum;
+        }
+        return arrayOut;
     }
     
     //Method 3: Takes a 2D array and adds  the two dimensions
-    private static double[] ArrayAdder(double arrayIn[][])
+    private static double[][] ArrayAdder(double arrayIn[][])
     {
-        double[] arrayOut = {0,0,0,0};
-        for(int i = 0; i<4; i++)
+        double[][] arrayOut = {{0,0,0,0},{0,0,0,0}};
+        for(int n = 0; n<4; n++)
         {
-            arrayOut[i] = (arrayIn[0][i] + arrayIn[1][i]);
+            arrayOut[0][n] = (arrayIn[0][n] + arrayIn[1][n]);
         }
         return arrayOut;
     }
     
     
-    // --------- MAIN -----------------------------
+    // --------- MAIN ----------------------------------------------------------------------------------------------
     public static void main(String[] args) throws IOException
     {
-        screen.println("FOR FOUR-VECTOR 1");
-        double[] fourVec1 = ArrayCompiler();
-        double[] fourVec2 = ArrayCompiler();
-        //Takes input array and outputs M and absP
-        double[] fourVecMandP1 = ArrayMandPextractor(fourVec1);
-        double[][] fourVec2D = {fourVec1,fourVec2};
-        double[] vecAdded = ArrayAdder(fourVec2D);
-        double[] fourVecMandP12 = ArrayMandPextractor(vecAdded);
-        screen.println("P is " +fourVecMandP1[0]+ " GeV and Momentum is " +fourVecMandP1[1] + " GeV.");
-        screen.println(vecAdded[0]+" "+ vecAdded[1]+" "+ vecAdded[2]+" "+ vecAdded[3]);
+        //Calculates necessary Variables using methods
+        double[][] fourVector_Array = ArrayCompiler();
+        double[][] MassMomentum_Array = ArrayMandPextractor(fourVector_Array);
+        double[][] fourVectorAdded_Array = ArrayAdder(fourVector_Array);
+        double[][] MassMomentumAdded_Array = ArrayMandPextractor(fourVectorAdded_Array);
+        //Crude output variables for debug purposes
+        screen.println(Arrays.deepToString(fourVector_Array));
+        screen.println(Arrays.deepToString(MassMomentum_Array));
+        screen.println(Arrays.deepToString(fourVectorAdded_Array));
+        screen.println(Arrays.deepToString(MassMomentumAdded_Array));        
     }
 }
