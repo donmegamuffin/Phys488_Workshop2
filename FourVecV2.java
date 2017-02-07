3c8cff02bd7a1ddd3dbb4e58437bdb54ad9ed72e
donmegamuffin <donmegamuffin@users.noreply.github.com>
2017-02-07 10:19:29 +0000

    2D Array adder implemented successfully
    TODO: Main method incomplete, variables still have horrible names, 
    methods generally OK but need further variable name cleanup

--- FourVecV2.java
+++ FourVecV2.java
@@ -5,7 +5,10 @@ class FourVecV2
 	BufferedReader (new InputStreamReader(System.in)) ;
     static PrintWriter screen = new PrintWriter( System.out, true);
     
+    
+    
     // ----------- METHODS ------------------------------  
+    
     // Method 1: Compiles inputs from user into an array
     private static double[] ArrayCompiler() throws IOException
     {
@@ -21,34 +24,39 @@ class FourVecV2
         }
         return outputArray;
     }
+    
     //Method 2: Takes an inputted Array and calculates the Rest Mass and Invariant Momentum
-    private static double[] AbsMomentumInvMass(double[] input4Vec)
+    private static double[] ArrayMandPextractor(double[] inputArray)
     {
-        double AbsMomentum = Math.sqrt(Math.pow(input4Vec[1],2) + Math.pow(input4Vec[2],2) + Math.pow(input4Vec[3],2));
-        double InvariantMass = Math.sqrt(Math.pow(input4Vec[0],2)- Math.pow(AbsMomentum,2));
-        double[] absMoInvMa = {AbsMomentum,InvariantMass};
-        return absMoInvMa;
+        double AbsMomentum = Math.sqrt(Math.pow(inputArray[1],2) + Math.pow(inputArray[2],2) + Math.pow(inputArray[3],2));
+        double InvariantMass = Math.sqrt(Math.pow(inputArray[0],2)- Math.pow(AbsMomentum,2));
+        double[] outputArray = {AbsMomentum,InvariantMass};
+        return outputArray;
     }
-    //Method 3: Takes a 2D array and adds  the two dimensions //TODO 
-    private static double [] ArrayAdder(double[][] twoDInput)
+    
+    //Method 3: Takes a 2D array and adds  the two dimensions
+    private static double[] ArrayAdder(double arrayIn[][])
     {
-        double[] oneDOutput = {0,0,0,0};
-        for(int n=0;n<4;n++)
+        double[] arrayOut = {0,0,0,0};
+        for(int i = 0; i<4; i++)
         {
-            oneDOutput[n] = (twoDInput[n][0]) + (twoDInput[n][1]);
+            arrayOut[i] = (arrayIn[0][i] + arrayIn[1][i]);
         }
-        return oneDOutput;
+        return arrayOut;
     }
+    
+    
     // --------- MAIN -----------------------------
     public static void main(String[] args) throws IOException
     {
         screen.println("FOR FOUR-VECTOR 1");
         double[] fourVec1 = ArrayCompiler();
-        double[] fourVec2 = {5,-4,0,0};
+        double[] fourVec2 = ArrayCompiler();
         //Takes input array and outputs M and absP
-        double[] fourVecMandP1 = AbsMomentumInvMass(fourVec1);
+        double[] fourVecMandP1 = ArrayMandPextractor(fourVec1);
         double[][] fourVec2D = {fourVec1,fourVec2};
         double[] vecAdded = ArrayAdder(fourVec2D);
+        double[] fourVecMandP12 = ArrayMandPextractor(vecAdded);
         screen.println("P is " +fourVecMandP1[0]+ " GeV and Momentum is " +fourVecMandP1[1] + " GeV.");
         screen.println(vecAdded[0]+" "+ vecAdded[1]+" "+ vecAdded[2]+" "+ vecAdded[3]);
     }
